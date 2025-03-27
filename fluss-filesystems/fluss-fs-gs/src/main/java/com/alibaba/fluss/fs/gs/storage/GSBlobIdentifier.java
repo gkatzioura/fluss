d1786cd4ac981d5c9faa75b861a94661dc3500aa
gkatzioura <gkatzioura@gmail.com>
2025-03-27 08:31:53 +0000

    630: Added blob identifier

---
+++ fluss-filesystems/fluss-fs-gs/src/main/java/com/alibaba/fluss/fs/gs/storage/GSBlobIdentifier.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2025 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.fluss.fs.gs.storage;
+
+import com.alibaba.fluss.utils.Preconditions;
+
+import com.google.cloud.storage.BlobId;
+
+import java.util.Objects;
+
+/* This file is based on source code of Apache Flink Project (https://flink.apache.org/), licensed by the Apache
+ * Software Foundation (ASF) under the Apache License, Version 2.0. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership. */
+
+/** An abstraction for the Google BlobId type. */
+public class GSBlobIdentifier {
+
+    /** The bucket name. */
+    public final String bucketName;
+
+    /** The object name, within the bucket. */
+    public final String objectName;
+
+    /**
+     * Construct an abstract blob identifier.
+     *
+     * @param bucketName The bucket name
+     * @param objectName The object name
+     */
+    public GSBlobIdentifier(String bucketName, String objectName) {
+        this.bucketName = Preconditions.checkNotNull(bucketName);
+        this.objectName = Preconditions.checkNotNull(objectName);
+    }
+
+    /**
+     * Get a Google blob id for this identifier, with generation=null.
+     *
+     * @return The BlobId
+     */
+    public BlobId getBlobId() {
+        return BlobId.of(bucketName, objectName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GSBlobIdentifier that = (GSBlobIdentifier) o;
+        return bucketName.equals(that.bucketName) && objectName.equals(that.objectName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bucketName, objectName);
+    }
+
+    @Override
+    public String toString() {
+        return "GSBlobIdentifier{"
+                + "bucketName='"
+                + bucketName
+                + '\''
+                + ", objectName='"
+                + objectName
+                + '\''
+                + '}';
+    }
+
+    /**
+     * Construct an abstract blob identifier from a Google BlobId.
+     *
+     * @param blobId The Google BlobId
+     * @return The abstract blob identifier
+     */
+    public static GSBlobIdentifier fromBlobId(BlobId blobId) {
+        return new GSBlobIdentifier(blobId.getBucket(), blobId.getName());
+    }
+}
