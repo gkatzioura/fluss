cffc7155d5e640aab320386bdfc094560305d228
gkatzioura <gkatzioura@gmail.com>
2025-03-29 23:08:55 +0000

    630: Added filesystem options

---
+++ fluss-filesystems/fluss-fs-gs/src/main/java/com/alibaba/fluss/fs/gs/GSFileSystemOptions.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2025 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.fluss.fs.gs;
+
+import com.alibaba.fluss.config.ConfigOption;
+import com.alibaba.fluss.config.Configuration;
+import com.alibaba.fluss.config.MemorySize;
+import com.alibaba.fluss.utils.Preconditions;
+
+import java.time.Duration;
+import java.util.Optional;
+
+import static com.alibaba.fluss.config.ConfigBuilder.key;
+
+/** The GS file system options. */
+public class GSFileSystemOptions {
+
+    /* Flink config option to set the bucket name for temporary blobs. */
+    public static final ConfigOption<String> WRITER_TEMPORARY_BUCKET_NAME =
+            key("gs.writer.temporary.bucket.name")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "This option sets the bucket name used by the recoverable writer to store temporary files. "
+                                    + "If not specified, temporary files are stored in the same bucket as the final file being written.");
+
+    /* Flink config option to set the chunk size for writing to GCS. */
+    public static final ConfigOption<MemorySize> WRITER_CHUNK_SIZE =
+            key("gs.writer.chunk.size")
+                    .memoryType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "This option sets the chunk size for writes to the underlying Google storage. If set, this must be a multiple "
+                                    + "of 256KB. If not set, writes will use Google's default chunk size.");
+
+    /* Flink config option to determine if entropy should be enabled in filesink gcs path. */
+    public static final ConfigOption<Boolean> ENABLE_FILESINK_ENTROPY =
+            key("gs.filesink.entropy.enabled")
+                    .booleanType()
+                    .defaultValue(Boolean.FALSE)
+                    .withDescription(
+                            "This option can be used to improve performance due to hotspotting "
+                                    + "issues on GCS. If this is enabled, entropy in the form of "
+                                    + "temporary object id will be injected in the beginning of "
+                                    + "gcs path of the temporary objects. The final object path "
+                                    + "remains unchanged.");
+
+    /**
+     * Flink config option to set the http connection timeout. It will be used by cloud-storage
+     * library.
+     */
+    public static final ConfigOption<Integer> GCS_HTTP_CONNECT_TIMEOUT =
+            key("gs.http.connect-timeout")
+                    .intType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "This option sets the timeout in milliseconds to establish a connection. GCS default will be used if not configured.");
+
+    /**
+     * Flink config option to set the http read timeout. It will be used by cloud-storage library.
+     */
+    public static final ConfigOption<Integer> GCS_HTTP_READ_TIMEOUT =
+            key("gs.http.read-timeout")
+                    .intType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "If configured this option sets the timeout in milliseconds to read data from an established connection. GCS default will be used if not configured");
+
+    /**
+     * Flink config option to set the http read timeout. It will be used by cloud-storage library.
+     */
+    public static final ConfigOption<Integer> GCS_RETRY_MAX_ATTEMPT =
+            key("gs.retry.max-attempt")
+                    .intType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "If configured this option defines the maximum number of retry attempts to perform. GCS default will be used if not configured. See GCS %s for more information. "
+                                    + "https://cloud.google.com/java/docs/reference/gax/latest/com.google.api.gax.retrying.RetrySettings#com_google_api_gax_retrying_RetrySettings_getMaxAttempts__ "
+                                    + "documentation");
+
+    public static final ConfigOption<Duration> GCS_RETRY_INIT_RPC_TIMEOUT =
+            key("gs.retry.init-rpc-timeout")
+                    .durationType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "If configured this option controls the timeout for the initial RPC. Subsequent calls will use this value adjusted according to the gs.retry.rpc-timeout-multiplier. GCS default will be used if not configured. See GCS %s for more information. "
+                                    + "https://cloud.google.com/java/docs/reference/gax/latest/com.google.api.gax.retrying.RetrySettings#com_google_api_gax_retrying_RetrySettings_getInitialRpcTimeout__ "
+                                    + "documentation");
+
+    public static final ConfigOption<Double> GCS_RETRY_RPC_TIMEOUT_MULTIPLIER =
+            key("gs.retry.rpc-timeout-multiplier")
+                    .doubleType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "If configured this option controls the change in delay before the next retry or poll. The timeout of the previous call is multiplied by the RpcTimeoutMultiplier to calculate the timeout for the next call. See GCS %s for more information. "
+                                    + "https://cloud.google.com/java/docs/reference/gax/latest/com.google.api.gax.retrying.RetrySettings#com_google_api_gax_retrying_RetrySettings_getRpcTimeoutMultiplier__ "
+                                    + "documentation");
+
+    public static final ConfigOption<Duration> GCS_RETRY_MAX_RPC_TIMEOUT =
+            key("gs.retry.max-rpc-timeout")
+                    .durationType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "If configured this option puts a limit on the value of the RPC timeout, so that the max rpc timeout can't increase the RPC timeout higher than this amount. GCS default will be used if not configured. See GCS %s for more information."
+                                    + "https://cloud.google.com/java/docs/reference/gax/latest/com.google.api.gax.retrying.RetrySettings#com_google_api_gax_retrying_RetrySettings_getMaxRpcTimeout__ "
+                                    + "documentation");
+
+    public static final ConfigOption<Duration> GCS_RETRY_TOTAL_TIMEOUT =
+            key("gs.retry.total-timeout")
+                    .durationType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "If configured total duration during which retries could be attempted. GCS default will be used if not configured. See GCS %s for more information. "
+                                    + "https://cloud.google.com/java/docs/reference/gax/latest/com.google.api.gax.retrying.RetrySettings#com_google_api_gax_retrying_RetrySettings_getTotalTimeout__ "
+                                    + "documentation");
+
+    /** The Flink configuration. */
+    private final Configuration flinkConfig;
+
+    /**
+     * Constructs an options instance.
+     *
+     * @param flinkConfig The Flink configuration
+     */
+    public GSFileSystemOptions(Configuration flinkConfig) {
+        this.flinkConfig = Preconditions.checkNotNull(flinkConfig);
+        this.flinkConfig
+                .getOptional(WRITER_CHUNK_SIZE)
+                .ifPresent(
+                        chunkSize ->
+                                Preconditions.checkArgument(
+                                        chunkSize.getBytes() > 0
+                                                && chunkSize.getBytes() % (256 * 1024) == 0,
+                                        "Writer chunk size must be greater than zero and a multiple of 256KB"));
+    }
+
+    /**
+     * The temporary bucket name to use for recoverable writes, if different from the final bucket
+     * name.
+     */
+    public Optional<String> getWriterTemporaryBucketName() {
+        return flinkConfig.getOptional(WRITER_TEMPORARY_BUCKET_NAME);
+    }
+
+    /** Timeout in millisecond to establish the connection. */
+    public Optional<Integer> getHTTPConnectionTimeout() {
+        return flinkConfig.getOptional(GCS_HTTP_CONNECT_TIMEOUT);
+    }
+
+    /** Timeout in millisecond to read content from connection. */
+    public Optional<Integer> getHTTPReadTimeout() {
+        return flinkConfig.getOptional(GCS_HTTP_READ_TIMEOUT);
+    }
+
+    public Optional<Integer> getMaxAttempts() {
+        return flinkConfig.getOptional(GCS_RETRY_MAX_ATTEMPT);
+    }
+
+    public Optional<org.threeten.bp.Duration> getInitialRpcTimeout() {
+        return flinkConfig
+                .getOptional(GCS_RETRY_INIT_RPC_TIMEOUT)
+                .map(timeout -> org.threeten.bp.Duration.ofMillis(timeout.toMillis()));
+    }
+
+    public Optional<Double> getRpcTimeoutMultiplier() {
+        return flinkConfig.getOptional(GCS_RETRY_RPC_TIMEOUT_MULTIPLIER);
+    }
+
+    public Optional<org.threeten.bp.Duration> getMaxRpcTimeout() {
+        return flinkConfig
+                .getOptional(GCS_RETRY_MAX_RPC_TIMEOUT)
+                .map(timeout -> org.threeten.bp.Duration.ofMillis(timeout.toMillis()));
+    }
+
+    public Optional<org.threeten.bp.Duration> getTotalTimeout() {
+        return flinkConfig
+                .getOptional(GCS_RETRY_TOTAL_TIMEOUT)
+                .map(timeout -> org.threeten.bp.Duration.ofMillis(timeout.toMillis()));
+    }
+
+    /** The chunk size to use for writes on the underlying Google WriteChannel. */
+    public Optional<MemorySize> getWriterChunkSize() {
+        return flinkConfig.getOptional(WRITER_CHUNK_SIZE);
+    }
+
+    /** Whether entropy insertion is enabled in filesink path. */
+    public Boolean isFileSinkEntropyEnabled() {
+        return flinkConfig.get(ENABLE_FILESINK_ENTROPY);
+    }
+
+    @Override
+    public String toString() {
+        return "GSFileSystemOptions{"
+                + "writerTemporaryBucketName="
+                + getWriterTemporaryBucketName()
+                + ", writerChunkSize="
+                + getWriterChunkSize()
+                + '}';
+    }
+}
--- fluss-filesystems/fluss-fs-gs/src/main/java/com/alibaba/fluss/fs/gs/GSFileSystemPlugin.java
+++ fluss-filesystems/fluss-fs-gs/src/main/java/com/alibaba/fluss/fs/gs/GSFileSystemPlugin.java
@@ -1,11 +1,13 @@
 /*
- * Copyright (c) 2025 Alibaba Group Holding Ltd.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,27 +18,40 @@
 
 package com.alibaba.fluss.fs.gs;
 
-import com.alibaba.fluss.config.ConfigBuilder;
 import com.alibaba.fluss.config.Configuration;
 import com.alibaba.fluss.fs.FileSystem;
 import com.alibaba.fluss.fs.FileSystemPlugin;
+import com.alibaba.fluss.fs.gs.utils.ConfigUtils;
+import com.alibaba.fluss.utils.Preconditions;
 
+import com.google.api.gax.retrying.RetrySettings;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.ServiceOptions;
 import com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem;
+import com.google.cloud.http.HttpTransportOptions;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.apache.hadoop.fs.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Optional;
 
 /** Simple factory for the Google Cloud Storage file system. */
 public class GSFileSystemPlugin implements FileSystemPlugin {
+
     private static final Logger LOG = LoggerFactory.getLogger(GSFileSystemPlugin.class);
 
     private static final String[] FLUSS_CONFIG_PREFIXES = {"gs.", "fs.gs."};
 
     private static final String HADOOP_CONFIG_PREFIX = "fs.gs.";
+
+    /** The scheme for the Google Storage file system. */
     public static final String SCHEME = "gs";
 
     @Override
@@ -45,52 +60,99 @@ public class GSFileSystemPlugin implements FileSystemPlugin {
     }
 
     @Override
-    public FileSystem create(URI fsUri, Configuration flussConfig) throws IOException {
-        org.apache.hadoop.conf.Configuration hadoopConfig = getHadoopConfiguration(flussConfig);
+    public FileSystem create(URI fsUri, Configuration configuration) throws IOException {
+        Preconditions.checkNotNull(configuration);
 
-        // create the Hadoop FileSystem
-        GoogleHadoopFileSystem fs = new GoogleHadoopFileSystem();
-        fs.initialize(getInitURI(fsUri, hadoopConfig), hadoopConfig);
-        Storage storage = StorageOptions.getDefaultInstance().getService();
-        ;
-        return new GSFileSystem(fs, storage, new GSFileSystemOptions(flussConfig));
-    }
+        ConfigUtils.ConfigContext configContext = new RuntimeConfigContext();
 
-    org.apache.hadoop.conf.Configuration getHadoopConfiguration(Configuration flussConfig) {
-        org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
-        if (flussConfig == null) {
-            return conf;
+        org.apache.hadoop.conf.Configuration hadoopConfig = ConfigUtils.getHadoopConfiguration(configuration, configContext);
+
+        LOG.info(
+                "Using Hadoop configuration {}", ConfigUtils.stringifyHadoopConfig(hadoopConfig));
+
+        GSFileSystemOptions fileSystemOptions = new GSFileSystemOptions(configuration);
+        LOG.info("Using file system options {}", fileSystemOptions);
+
+        StorageOptions.Builder storageOptionsBuilder = StorageOptions.newBuilder();
+        storageOptionsBuilder.setTransportOptions(getHttpTransportOptions(fileSystemOptions));
+        storageOptionsBuilder.setRetrySettings(getRetrySettings(fileSystemOptions));
+
+        Optional<GoogleCredentials> credentials =
+                ConfigUtils.getStorageCredentials(hadoopConfig, configContext);
+        credentials.ifPresent(storageOptionsBuilder::setCredentials);
+
+        ConfigUtils.getGcsRootUrl(hadoopConfig).ifPresent(storageOptionsBuilder::setHost);
+
+        Storage storage = storageOptionsBuilder.build().getService();
+
+        Preconditions.checkNotNull(fsUri);
+
+        Preconditions.checkNotNull(fsUri);
+
+        GoogleHadoopFileSystem googleHadoopFileSystem = new GoogleHadoopFileSystem();
+        try {
+            googleHadoopFileSystem.initialize(fsUri, hadoopConfig);
+        } catch (IOException ex) {
+            throw new IOException("Failed to initialize GoogleHadoopFileSystem", ex);
         }
 
-        for (String key : flussConfig.keySet()) {
-            for (String prefix : FLUSS_CONFIG_PREFIXES) {
-                if (key.startsWith(prefix)) {
-                    String newKey = HADOOP_CONFIG_PREFIX + key.substring(prefix.length());
-                    String newValue =
-                            flussConfig.getString(
-                                    ConfigBuilder.key(key).stringType().noDefaultValue(), null);
-                    conf.set(newKey, newValue);
+        return new GSFileSystem(googleHadoopFileSystem, storage, fileSystemOptions);
+    }
 
-                    LOG.debug(
-                            "Adding Fluss config entry for {} as {} to Hadoop config", key, newKey);
-                }
+    private HttpTransportOptions getHttpTransportOptions(GSFileSystemOptions fileSystemOptions) {
+        Optional<Integer> connectionTimeout = fileSystemOptions.getHTTPConnectionTimeout();
+        Optional<Integer> readTimeout = fileSystemOptions.getHTTPReadTimeout();
+        HttpTransportOptions.Builder httpTransportOptionsBuilder =
+                HttpTransportOptions.newBuilder();
+        connectionTimeout.ifPresent(httpTransportOptionsBuilder::setConnectTimeout);
+        readTimeout.ifPresent(httpTransportOptionsBuilder::setReadTimeout);
+        return httpTransportOptionsBuilder.build();
+    }
+
+    private RetrySettings getRetrySettings(GSFileSystemOptions fileSystemOptions) {
+        Optional<Integer> maxAttempts = fileSystemOptions.getMaxAttempts();
+        Optional<org.threeten.bp.Duration> initialRpcTimeout =
+                fileSystemOptions.getInitialRpcTimeout();
+        Optional<Double> rpcTimeoutMultiplier = fileSystemOptions.getRpcTimeoutMultiplier();
+        Optional<org.threeten.bp.Duration> maxRpcTimeout = fileSystemOptions.getMaxRpcTimeout();
+        Optional<org.threeten.bp.Duration> totalTimeout = fileSystemOptions.getTotalTimeout();
+        RetrySettings.Builder retrySettingsBuilder =
+                ServiceOptions.getDefaultRetrySettings().toBuilder();
+
+        maxAttempts.ifPresent(retrySettingsBuilder::setMaxAttempts);
+        initialRpcTimeout.ifPresent(retrySettingsBuilder::setInitialRpcTimeout);
+        rpcTimeoutMultiplier.ifPresent(retrySettingsBuilder::setRpcTimeoutMultiplier);
+        maxRpcTimeout.ifPresent(retrySettingsBuilder::setMaxRpcTimeout);
+        totalTimeout.ifPresent(retrySettingsBuilder::setTotalTimeout);
+        return retrySettingsBuilder.build();
+    }
+
+    /** Config context implementation used at runtime. */
+    private static class RuntimeConfigContext implements ConfigUtils.ConfigContext {
+
+        @Override
+        public Optional<String> getenv(String name) {
+            return Optional.ofNullable(System.getenv(name));
+        }
+
+        @Override
+        public org.apache.hadoop.conf.Configuration loadHadoopConfigFromDir(String configDir) {
+            org.apache.hadoop.conf.Configuration hadoopConfig =
+                    new org.apache.hadoop.conf.Configuration();
+            hadoopConfig.addResource(new Path(configDir, "core-default.xml"));
+            hadoopConfig.addResource(new Path(configDir, "core-site.xml"));
+            hadoopConfig.reloadConfiguration();
+            return hadoopConfig;
+        }
+
+        @Override
+        public GoogleCredentials loadStorageCredentialsFromFile(String credentialsPath) {
+            try (FileInputStream credentialsStream = new FileInputStream(credentialsPath)) {
+                return GoogleCredentials.fromStream(credentialsStream);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
             }
         }
-        return conf;
     }
 
-    private URI getInitURI(URI fsUri, org.apache.hadoop.conf.Configuration hadoopConfig) {
-        final String scheme = fsUri.getScheme();
-        final String authority = fsUri.getAuthority();
-
-        if (scheme == null && authority == null) {
-            fsUri = org.apache.hadoop.fs.FileSystem.getDefaultUri(hadoopConfig);
-        } else if (scheme != null && authority == null) {
-            URI defaultUri = org.apache.hadoop.fs.FileSystem.getDefaultUri(hadoopConfig);
-            if (scheme.equals(defaultUri.getScheme()) && defaultUri.getAuthority() != null) {
-                fsUri = defaultUri;
-            }
-        }
-        return fsUri;
-    }
 }
