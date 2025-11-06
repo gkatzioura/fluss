a3d08727cf399e8f0d13ebebe0f4d52fe5c0f0e3
gkatzioura <gkatzioura@gmail.com>
2025-11-06 08:40:01 +0000

    azure: support

---
+++ fluss-filesystems/fluss-fs-abfs/src/main/java/org/apache/fluss/fs/abfs/AzureFileSystem.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.fluss.fs.abfs;
+
+import org.apache.fluss.fs.hdfs.HadoopFileSystem;
+import org.apache.fluss.fs.token.ObtainedSecurityToken;
+
+import org.apache.hadoop.conf.Configuration;
+
+import java.io.IOException;
+
+public class AzureFileSystem extends HadoopFileSystem {
+
+    private final String scheme;
+    private final Configuration conf;
+
+    /**
+     * Wraps the given Hadoop File System object as a Flink File System object. The given Hadoop
+     * file system object is expected to be initialized already.
+     *
+     * @param hadoopFileSystem The Hadoop FileSystem that will be used under the hood.
+     */
+    public AzureFileSystem(
+            String scheme, org.apache.hadoop.fs.FileSystem hadoopFileSystem, Configuration conf) {
+        super(hadoopFileSystem);
+        this.scheme = scheme;
+        this.conf = conf;
+    }
+
+    @Override
+    public ObtainedSecurityToken obtainSecurityToken() throws IOException {
+        throw new UnsupportedOperationException("Not impl.");
+    }
+}
