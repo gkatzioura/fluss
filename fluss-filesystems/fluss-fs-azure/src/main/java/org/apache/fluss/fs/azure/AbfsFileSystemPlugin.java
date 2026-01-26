e01d30830dfdb5fdcaf027e6ab75338eec616cda
gkatzioura <gkatzioura@gmail.com>
2026-01-26 07:27:09 +0000

    1311: switched to AzureFileSystemPlugin class

--- fluss-filesystems/fluss-fs-azure/src/main/java/org/apache/fluss/fs/azure/AbfsFileSystemPlugin.java
+++ fluss-filesystems/fluss-fs-azure/src/main/java/org/apache/fluss/fs/azure/AbfsFileSystemPlugin.java
@@ -17,24 +17,6 @@
 
 package org.apache.fluss.fs.azure;
 
-import org.apache.fluss.config.ConfigBuilder;
-import org.apache.fluss.config.Configuration;
-import org.apache.fluss.fs.FileSystem;
-import org.apache.fluss.fs.FileSystemPlugin;
-import org.apache.fluss.fs.azure.token.AzureDelegationTokenReceiver;
-
-import org.apache.hadoop.fs.azurebfs.AzureBlobFileSystem;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.URI;
-import java.util.Objects;
-
-import static org.apache.fluss.fs.azure.token.AzureDelegationTokenProvider.ACCOUNT_KEY;
-import static org.apache.fluss.fs.azure.token.AzureDelegationTokenProvider.CLIENT_ID;
-import static org.apache.fluss.fs.azure.token.AzureDelegationTokenReceiver.PROVIDER_CONFIG_NAME;
-
 /**
  * FileSystem plugin for Azure Blob Storage using the ABFS (Azure Blob File System) driver.
  * Registered for the {@code abfs://} scheme.
@@ -44,88 +26,10 @@ import static org.apache.fluss.fs.azure.token.AzureDelegationTokenReceiver.PROVI
  *
  * <p>URI format: {@code abfs://<container>@<storage-account>.dfs.core.windows.net/<path>}
  */
-public class AbfsFileSystemPlugin implements FileSystemPlugin {
-
-    private static final Logger LOG = LoggerFactory.getLogger(AbfsFileSystemPlugin.class);
-
-    private static final String[] FLUSS_CONFIG_PREFIXES = {"fs.azure."};
-
-    private static final String HADOOP_CONFIG_PREFIX = "fs.azure.";
+public class AbfsFileSystemPlugin extends AzureFileSystemPlugin {
 
     @Override
     public String getScheme() {
         return "abfs";
-    }
-
-    @Override
-    public FileSystem create(URI fsUri, Configuration flussConfig) throws IOException {
-        org.apache.hadoop.conf.Configuration hadoopConfig = getHadoopConfiguration(flussConfig);
-
-        setCredentialProvider(hadoopConfig);
-
-        // create the Azure Hadoop FileSystem
-        org.apache.hadoop.fs.FileSystem fs = new AzureBlobFileSystem();
-        fs.initialize(getInitURI(fsUri, hadoopConfig), hadoopConfig);
-        return new AzureFileSystem(getScheme(), fs, hadoopConfig);
-    }
-
-    private void setCredentialProvider(org.apache.hadoop.conf.Configuration hadoopConfig) {
-        if (hadoopConfig.get(ACCOUNT_KEY) == null) {
-            if (Objects.equals(getScheme(), "abfs")) {
-                AzureDelegationTokenReceiver.updateHadoopConfig(hadoopConfig);
-            } else if (Objects.equals(getScheme(), "abfss")) {
-                AzureDelegationTokenReceiver.updateHadoopConfig(hadoopConfig);
-            } else if (Objects.equals(getScheme(), "wasb")) {
-                AzureDelegationTokenReceiver.updateHadoopConfig(hadoopConfig);
-            } else if (Objects.equals(getScheme(), "wasbs")) {
-                AzureDelegationTokenReceiver.updateHadoopConfig(hadoopConfig);
-            } else {
-                throw new IllegalArgumentException("Unsupported scheme: " + getScheme());
-            }
-            LOG.info(
-                    "{} is not set, using credential provider {}.",
-                    CLIENT_ID,
-                    hadoopConfig.get(PROVIDER_CONFIG_NAME));
-        } else {
-            LOG.info("{} is set, using provided account key.", ACCOUNT_KEY);
-        }
-    }
-
-    org.apache.hadoop.conf.Configuration getHadoopConfiguration(Configuration flussConfig) {
-        org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
-        if (flussConfig == null) {
-            return conf;
-        }
-
-        for (String key : flussConfig.keySet()) {
-            for (String prefix : FLUSS_CONFIG_PREFIXES) {
-                if (key.startsWith(prefix)) {
-                    String newKey = HADOOP_CONFIG_PREFIX + key.substring(prefix.length());
-                    String newValue =
-                            flussConfig.getString(
-                                    ConfigBuilder.key(key).stringType().noDefaultValue(), null);
-                    conf.set(newKey, newValue);
-
-                    LOG.debug(
-                            "Adding Fluss config entry for {} as {} to Hadoop config", key, newKey);
-                }
-            }
-        }
-        return conf;
-    }
-
-    private URI getInitURI(URI fsUri, org.apache.hadoop.conf.Configuration hadoopConfig) {
-        final String scheme = fsUri.getScheme();
-        final String authority = fsUri.getAuthority();
-
-        if (scheme == null && authority == null) {
-            fsUri = org.apache.hadoop.fs.FileSystem.getDefaultUri(hadoopConfig);
-        } else if (scheme != null && authority == null) {
-            URI defaultUri = org.apache.hadoop.fs.FileSystem.getDefaultUri(hadoopConfig);
-            if (scheme.equals(defaultUri.getScheme()) && defaultUri.getAuthority() != null) {
-                fsUri = defaultUri;
-            }
-        }
-        return fsUri;
     }
 }
